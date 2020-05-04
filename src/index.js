8173e54587df81303c85b8b0abed83027f764a07
errgou <qiyi.li@aol.com>
2020-05-04 22:25:03 +0800

    useState

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root')
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let _state = []
+let index = 0
+const myUseState = initialValue => {
+  const currentIndex = index
+  _state[currentIndex] = _state[currentIndex] === undefined ? initialValue : _state[currentIndex]
+  const setState = (newValue) => {
+    _state[currentIndex] = newValue
+    console.log('_state')
+    console.log(_state)
+    render()
+  }
+  index += 1
+  return [_state[currentIndex], setState]
+}
+
+const render = () => {
+  index = 0
+  ReactDOM.render(<App/>, rootElement)
+}
+
+function App() {
+  const [n, setN] = myUseState(0)
+  const [m, setM] = myUseState(0)
+  return (
+    <div className="App">
+      <p>{n}</p>
+      <p>
+        <button onClick={() => setN(n + 1)}>n+1</button>
+      </p>
+      <p>{m}</p>
+      <p>
+        <button onClick={() => setM(m + 1)}>m+1</button>
+      </p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App/>, rootElement)
